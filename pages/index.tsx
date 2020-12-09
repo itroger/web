0c2adf34582b8b19cf0028b7bf777127ae383f8b
itroger <743163143@qq.com>
2020-12-09 18:17:41 +0800

    next

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd"
+import styles from './index.less'
+
+const HomePage = () => {
+  return <div className={styles.homePage}>
+    <h1>Welcome to Next.js!</h1>
+    <Button>按钮</Button>
+  </div>
+}
+
+export default HomePage
