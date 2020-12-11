85649e9d50067b8ddf071c3ec2f28e41e52ea90b
itroger <743163143@qq.com>
2020-12-11 18:32:03 +0800

    next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,39 @@
-const withLess = require('@zeit/next-less')
+const cssLoaderConfig = require('@zeit/next-css/css-loader-config')
 
-module.exports = withLess({
-    cssModules: true,
-    options: {
-        lessOptions: {
-            javascriptEnabled: true
+module.exports = (nextConfig = {}) => {
+    return Object.assign({}, nextConfig, {
+        webpack(config, options) {
+            const { dev, isServer } = options
+            const {
+                cssModules,
+                cssLoaderOptions,
+                lessLoaderOptions = {}
+            } = nextConfig
+
+            options.defaultLoaders.less = cssLoaderConfig(config, {
+                extensions: ['less'],
+                cssModules,
+                cssLoaderOptions,
+                dev,
+                isServer,
+                loaders: [
+                    {
+                        loader: 'less-loader',
+                        options: lessLoaderOptions
+                    }
+                ]
+            })
+
+            config.module.rules.push({
+                test: /\.less/,
+                use: options.defaultLoaders.less
+            })
+
+            if (typeof nextConfig.webpack === 'function') {
+                return nextConfig.webpack(config, options)
+            }
+
+            return config
         }
-    }
-})
+    })
+}
