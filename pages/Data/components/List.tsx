1bcbac6ec97684e3d8e79eaf341574a15be15ff4
itroger <743163143@qq.com>
2020-12-21 17:13:32 +0800

    编程列表

--- pages/Data/components/List.tsx
+++ pages/Data/components/List.tsx
@@ -6,10 +6,10 @@ const List: React.FC<ListProps> = props => {
   const { id, name, category = [], level, style, className, title } = props
 
   return <div className={`${styles.list} ${className}`} style={style}>
-    <div>{id}</div>
-    <div>{name}</div>
-    <div>{title ? category[0] : category.map(item => <span key={item} className={styles.category}>{item}</span>)}</div>
-    <div>{level}</div>
+    <p>{id}</p>
+    <p>{name}</p>
+    <p>{title ? category[0] : category.map(item => <span key={item} className={styles.category}>{item}</span>)}</p>
+    <p>{level}</p>
   </div>
 }
 
