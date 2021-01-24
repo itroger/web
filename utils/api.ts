8f7567903b1f078b95f72c8b8149b0a20c56c0fd
itroger <743163143@qq.com>
2021-01-24 20:39:48 +0800

    api

--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,5 @@
 import { AttributeType } from 'pages/data'
-const apiHost = 'http://localhost:8000/'
+const apiHost = 'http://localhost:8000/api/'
 
 export const documentQuery = async (attribute: AttributeType) => {
   return (await fetch(`${apiHost}documentQuery?attribute=${attribute}`)).json()
