4e957d1a4b5238e8d65961f9240b599d7a28344b
itroger <itroger@outlook.com>
2021-02-01 23:09:45 +0800

    socket.io

--- components/Socket/chat.tsx
+++ components/Socket/chat.tsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 import styles from './index.less'
 import { ChatProps, MessageType } from 'components/socket'
 
-const socket = io('https://itroger.cn:3000/', {
+const socket = io('https://itroger.cn/socket', {
   'transports': ['websocket', 'polling']
 })
 
